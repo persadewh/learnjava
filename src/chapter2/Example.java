5ab30264478f3f3c756dd0829174237eb30c7279
persadewh <persadewh@126.com>
2018-01-02 15:07:36 +0800

    init project for learn java

---
+++ src/chapter2/Example.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+/**
+ * @Author RayWei
+ * @Date 2018/1/2 15:03
+ * This is a simple Java program.
+ */
+
+public class Example {
+
+    public static void main(String[] args)
+    {
+        System.out.println("This is a simple Java program.");
+    }
+}
